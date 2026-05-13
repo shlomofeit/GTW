be6f846c7e866e06f8cfdf48f2d43c1857d1a334
Shlomo Feit <11.5sfeit@gmail.com>
2026-05-13 11:17:48 +0300

    Add the display_game_status function

--- main.py
+++ main.py
@@ -7,3 +7,18 @@ def random_word(words: list):
         return f'No words in the json file.'
     
     return random.choice(words).upper()
+
+
+def display_game_status(secret_word, guessed_letters, unguessed_letters, attempts):
+
+    word_for_display = ' '.join([letter if letter in guessed_letters else '_' for letter in secret_word])
+
+    remaining = 15 - attempts
+
+    print("\n" + '=' * 45)
+    print(f' STATUS: {remaining} Attempts left | Guessed: {', '.join(guessed_letters)}')
+    print('=' * 45)
+    print(f'\n{12 * ' '}>>  {word_for_display}  <<\n')
+    print('=' * 45)
+    print(f'Wrong letters that you tried: {' '.join(unguessed_letters)}')
+    print('=' * 45)
